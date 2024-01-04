283d7e347ea6968619f6d4335dec9d92a0623b6e
Martin Dieck <krutterlax@gmail.com>
2024-01-03 18:09:31 -0600

    Added Bullet Deletion Conditions

--- bullet.gd
+++ bullet.gd
@@ -1,5 +1,13 @@
 extends Area2D
 
+var travelled_distance
+const SPEED = 1000
+const RANGE = 1200
+
 func _physics_process(delta):
 	var direction = Vector2.RIGHT.rotated(rotation)
-	position += direction * 1000 * delta
+	position += direction * SPEED * delta
+	
+	travelled_distance += SPEED * delta
+	if travelled_distance > RANGE:
+		queue_free()
