05714e290c59f8b08703e8d4b905e53e6f8f41d1
Martin Dieck <krutterlax@gmail.com>
2024-01-08 16:11:04 -0600

    Adding Player Damage Detection
    First look into custom signals

--- player.gd
+++ player.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+signal health_depleted
+
+var health = 100.0
+const DAMAGE_RATE = 5.0
+
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
@@ -9,3 +14,9 @@ func _physics_process(delta):
 		%HappyBoo.play_walk_animation()
 	else:
 		%HappyBoo.play_idle_animation()
+
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= overlapping_mobs.size() * DAMAGE_RATE * delta
+		if health <= 0.0:
+			health_depleted.emit()
