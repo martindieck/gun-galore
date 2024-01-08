46b8e0d3281142058b8d849d164ad9211108f985
Martin Dieck <krutterlax@gmail.com>
2024-01-08 15:54:30 -0600

    Added Rotation fix to Shooting

--- gun.gd
+++ gun.gd
@@ -10,6 +10,7 @@ func shoot():
 	const BULLET = preload("res://bullet.tscn")
 	var new_bullet = BULLET.instantiate()
 	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
 
 func _on_timer_timeout():
