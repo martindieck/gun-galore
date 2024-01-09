7754d6dc9d44c1f81e53710b102b69430018ef0e
Martin Dieck <krutterlax@gmail.com>
2024-01-08 18:02:29 -0600

    Added Level Up Mechanic

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 signal health_depleted
 
 var health = 100.0
+var level = 1
 const DAMAGE_RATE = 5.0
 
 func _physics_process(delta):
@@ -15,9 +16,16 @@ func _physics_process(delta):
 	else:
 		%HappyBoo.play_idle_animation()
 
+	if Global.killcount / 10 == level:
+		level_up()
+	
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
 		health -= overlapping_mobs.size() * DAMAGE_RATE * delta
 		%HealthBar.value = health
 		if health <= 0.0:
 			health_depleted.emit()
+
+func level_up():
+	get_node("Gun/%ShootTimer").wait_time -= 0.1
+	level += 1
